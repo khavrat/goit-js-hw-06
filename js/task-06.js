97f0b6972abc5e8f360734f02e392e301e2f462b
khavrat <ihavrat@gmail.com>
2022-12-09 08:21:50 +0200

    solved the task 06

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const inputRef = document.querySelector("#validation-input");
+inputRef.addEventListener("blur", onInputBlur);
+
+function onInputBlur() {
+      if (inputRef.value.length > inputRef.dataset.length) {
+        inputRef.classList.add("invalid");
+        inputRef.classList.remove("valid");
+      } else {
+        inputRef.classList.remove("invalid");
+        inputRef.classList.add("valid");
+    };
+};
