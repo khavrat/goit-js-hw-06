c173182bc032e88108de27ddc500192eccd1abc3
khavrat <ihavrat@gmail.com>
2022-12-09 06:22:57 +0200

    solved the task 04

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const counterRef = document.querySelector("#counter");
+const decrementBtnRef = document.querySelector("[data-action=decrement]");
+const valueRef = document.querySelector("#value");
+const incrementBtnRef = document.querySelector("[data-action=increment]");
+
+let counterValue = 0;
+
+decrementBtnRef.addEventListener('click', function () {
+    const result = counterValue -= 1;
+    valueRef.textContent = result;
+});
+
+incrementBtnRef.addEventListener("click", function () {
+  const result = counterValue += 1;
+    valueRef.textContent = result;
+});
+
+
