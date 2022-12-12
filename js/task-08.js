99f9e2dae881f990c0a9e8f4bb12e1d8818fbc8f
khavrat <ihavrat@gmail.com>
2022-12-12 06:54:36 +0200

    solved the task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in this form completely!");
+  }
+
+  const formData = { email: email.value, password: password.value };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
