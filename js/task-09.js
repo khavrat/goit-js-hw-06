9e5db2974fe6a88673ae7488528dc8b7618dbded
khavrat <ihavrat@gmail.com>
2022-12-12 10:49:55 +0200

    solved the task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const btnChangeColorRef = document.querySelector('.change-color');
+const spanColorValueRef = document.querySelector('.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnChangeColorRef.addEventListener('click', toChangeColor);
+
+function toChangeColor(event) {
+  let currentColor = document.body.style.backgroundColor = getRandomHexColor();
+  spanColorValueRef.innerHTML = currentColor;
 }
