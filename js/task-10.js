00f204ec7271a0b4187760f35602945d44aa7810
khavrat <ihavrat@gmail.com>
2022-12-12 17:20:27 +0200

    solved the task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
+const divBoxesRef = document.querySelector("#boxes");
+const inputRef = document.querySelector("input");
+const btnCreateRef = document.querySelector("button[data-create]");
+const btnDestroyRef = document.querySelector("button[data-destroy]");
+
+btnCreateRef.addEventListener("click", () => {
+  createBoxes(inputRef.value);
+});
+
+btnDestroyRef.addEventListener("click", () => {
+  divBoxesRef.innerHTML = "";
+  inputRef.value = "";
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+function createBoxes(amount) {
+  const boxDivElements = [];
+  for (let i = 0; i < amount; i += 1) {
+    const divElement = document.createElement("div");
+    divElement.style.width = `${30 + 10 * i}px`;
+    divElement.style.height = `${30 + 10 * i}px`;
+    divElement.style.backgroundColor = getRandomHexColor();
+    boxDivElements.push(divElement);
+  };
+  return divBoxesRef.append(...boxDivElements);
+};
